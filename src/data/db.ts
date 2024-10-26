1dea8356ac4114d1b898fd380132997a7068914a
paul1n4 <paulinevaleroc@gmail.com>
2024-10-26 13:31:59 -0400

    first commit

---
+++ src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from '../types'
+
+export const menuItems : MenuItem[] = [
+  {
+    "id": 1,
+    "name": "Small Wood-Fired Pizza",
+    "price": 30
+  },
+  {
+    "id": 2,
+    "name": "Medium Wood Fired Pizza",
+    "price": 50
+  },
+  {
+    "id": 3,
+    "name": "Slice of Lemon Pie",
+    "price": 30
+  },
+  {
+    "id": 4,
+    "name": "Chocolate Cake Slice",
+    "price": 30
+  },
+  {
+    "id": 5,
+    "name": "Orange juice",
+    "price": 15
+  },
+  {
+    "id": 6,
+    "name": "Large Wood Fired Pizza",
+    "price": 70
+  },
+  {
+    "id": 7,
+    "name": "Rib Eye 800g",
+    "price": 100
+  },
+  {
+    "id": 8,
+    "name": "Apple Juice",
+    "price": 15
+  },
+  {
+    "id": 9,
+    "name": "Tequila",
+    "price": 40
+  },
+  {
+    "id": 10,
+    "name": "Slice of Cheesecake",
+    "price": 30
+  },
+  {
+    "id": 11,
+    "name": "Americano",
+    "price": 20
+  },
+  {
+    "id": 12,
+    "name": "Cappuccino",
+    "price": 40
+  }
+]
